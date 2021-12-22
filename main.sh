3dc4f175c2c802bf4e1e182c82960817109f06dd
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2021-12-22 12:31:37 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-sudo dpkg -i raw.githubusercontent.com/pwn20wndstuff/Undecimus/master/apt/cydia_1.1.32%7Eb16_iphoneos-arm.deb
+dpkg -i raw.githubusercontent.com/pwn20wndstuff/Undecimus/master/apt/cydia_1.1.32%7Eb16_iphoneos-arm.deb
